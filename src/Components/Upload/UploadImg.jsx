121379806cc9dc5328fac08ef8c28d382123769b
3bderhman <abderhmansaid58@gmail.com>
2022-05-20 20:12:21 +0200

    second commit

--- src/Components/Upload/UploadImg.jsx
+++ src/Components/Upload/UploadImg.jsx
@@ -3,11 +3,11 @@ function UploadImg() {
 
   const [file, setFile] = useState(null);
   const [error, setError] = useState(null);
-
-  const types = ['image/png', 'image/jpeg'];
-
-  const changeHandler = (e) => {
-    let selected = e.target.files[0];
+  const [selectedImg, setSelectedImg] = useState(null);
+  
+  const imageHandler = (img) => {
+    const types = ['image/png', 'image/jpeg'];
+    let selected = img;
     if (selected && types.includes(selected.type)) {
       setFile(selected);
       setError(null);
@@ -16,6 +16,18 @@ function UploadImg() {
       setError('Please select an image file (png or jpg)');
     }
   }
+  const changeHandler = (e) => {
+    const photo = e.target.files[0]
+    imageHandler(photo)
+    const reader = new FileReader();
+    reader.onload = () =>{
+      if(reader.readyState === 2){
+        setSelectedImg(reader.result)
+      }
+    }
+    reader.readAsDataURL(photo)
+  };
+
   return (
     <>
       <div className='text-center'>
@@ -25,11 +37,13 @@ function UploadImg() {
         </label>
         <div>
           { error && <div className="text-danger">{ error }</div>}
-          { file && <div>{ file.name }</div> }
         </div>
       </div>
+      { selectedImg && <div>
+          <p className='h4 border-bottom'>uploaded Img</p>
+          <img src={selectedImg} alt={ file.name } className='selectedImg' />
+      </div>}
     </>
   )
 }
-
 export default UploadImg
